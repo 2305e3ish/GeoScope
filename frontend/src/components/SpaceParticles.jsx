1712d41463f946f8d75b2db9bc4864b03b97f6f5
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2025-08-25 16:03:25 +0530

    search page frontend

---
+++ frontend/src/components/SpaceParticles.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function SpaceParticles() {
+  return (
+    <iframe
+      src='https://my.spline.design/elegantbeautyofdarkaesthetics-1hRzZit6NzyuDlNUhUots2pg/' 
+        frameborder='0' 
+        width='100%' 
+        height='100%'
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: 0,
+        pointerEvents: "none",
+      }}
+      title="Spline 3D Background"
+      allowFullScreen
+    />
+  );
+}
